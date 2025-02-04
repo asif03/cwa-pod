e98d233b9d6306ba1d4363c3dab53521c4b575e7
asif03 <babu033045@gmail.com>
2025-02-04 15:23:00 +0600

    update code

--- src/pages/OrderDetails.tsx
+++ src/pages/OrderDetails.tsx
@@ -64,58 +64,144 @@ const OrderDetails = () => {
           </button>
         </div>
       </div>
-      <div className="flex w-full flex-1 flex-col rounded-xl bg-white p-5 drop-shadow-xl">
-        <h4>Products</h4>
-        <div className="overflow-x-scroll rounded-xl border lg:overflow-hidden">
-          <table className="w-full table-auto">
-            <tbody>
-              <tr>
-                <td>
-                  <div className="border p-4">
-                    <img
-                      src={productImg}
-                      alt="Product Image"
-                      width={60}
-                      height={60}
-                    />
-                  </div>
-                </td>
-                <td className="p-4">
-                  <div className="flex flex-col gap-2">
-                    <p className="font-bold">
-                      Product Long Title To Test Long Title in the Product List
-                      Table
-                    </p>
-                    <div className="flex flex-row items-center justify-start gap-2">
-                      <span className="badge">Front</span>
-                      <span className="badge">Back</span>
-                      <span className="badge">Left</span>
-                      <span className="badge">Right</span>
-                    </div>
-                  </div>
-                </td>
-                <td>Sep 25 2024 10:51am</td>
-                <td>Mr. John Doe</td>
-                <td className="font-bold">$1000.00</td>
-              </tr>
-              <tr>
-                <td className="w-16">
-                  <div className="border p-4">
-                    <img
-                      src={productImg}
-                      alt="Product Image"
-                      width={60}
-                      height={60}
-                    />
-                  </div>
-                </td>
-                <td></td>
-                <td>Sep 25 2024 10:51am</td>
-                <td>Mr. John Doe</td>
-                <td className="font-bold">$1000.00</td>
-              </tr>
-            </tbody>
-          </table>
+
+      <div className="flex flex-col gap-5 lg:flex-row">
+        <div className="flex w-full flex-col gap-5 lg:w-3/4">
+          <div className="flex w-full flex-col gap-5 rounded-xl bg-white p-5 drop-shadow-xl">
+            <h4>Products</h4>
+            <div className="overflow-x-scroll rounded-xl border lg:overflow-hidden">
+              <table className="w-full table-auto">
+                <tbody>
+                  <tr>
+                    <td className="w-20 pl-2">
+                      <div className="border p-2">
+                        <img
+                          src={productImg}
+                          alt="Product Image"
+                          width={80}
+                          height={80}
+                        />
+                      </div>
+                    </td>
+                    <td className="p-4">
+                      <div className="flex flex-col gap-2">
+                        <p className="font-bold">
+                          Men’s Staple Tank Top | Bella + Canvas 3480 (Black /
+                          M)
+                        </p>
+                        <p className="text-xs font-normal">
+                          Men’s Staple Tank Top | Bella + Canvas 3480
+                        </p>
+                        <span className="badge w-[134px] rounded-full bg-[#DEE1E8] px-4 text-center">
+                          Black / M
+                        </span>
+                      </div>
+                    </td>
+                    <td>$15.00 X 3</td>
+                    <td>$45.00</td>
+                  </tr>
+                  <tr>
+                    <td className="pl-2">
+                      <div className="border p-2">
+                        <img
+                          src={productImg}
+                          alt="Product Image"
+                          width={80}
+                          height={80}
+                        />
+                      </div>
+                    </td>
+                    <td className="p-4">
+                      <div className="flex flex-col gap-2">
+                        <p className="font-bold">
+                          Men’s Staple Tank Top | Bella + Canvas 3480 (Black /
+                          M)
+                        </p>
+                        <p className="text-xs font-normal">
+                          Men’s Staple Tank Top | Bella + Canvas 3480
+                        </p>
+                        <span className="badge w-[134px] rounded-full bg-[#DEE1E8] px-4 text-center">
+                          Black / M
+                        </span>
+                      </div>
+                    </td>
+                    <td>$15.00 X 3</td>
+                    <td>$45.00</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className="flex w-full flex-col gap-5 rounded-xl bg-white p-5 drop-shadow-xl">
+            <h4>Pricing Summery</h4>
+            <div className="overflow-x-scroll lg:overflow-hidden">
+              <table className="w-full table-auto">
+                <tbody>
+                  <tr className="border-0">
+                    <td className="w-20">Subtotal</td>
+                    <td>6 Items</td>
+                    <td>$135.00</td>
+                  </tr>
+                  <tr className="border-0">
+                    <td className="w-20">Shipping</td>
+                    <td>Shipping Method Information</td>
+                    <td>$135.00</td>
+                  </tr>
+                  <tr className="border-0">
+                    <td className="w-20">Tax</td>
+                    <td>Tax Details with country name</td>
+                    <td>$15.00</td>
+                  </tr>
+                  <tr>
+                    <td className="w-20 font-bold">Total</td>
+                    <td></td>
+                    <td className="font-bold">$15.00</td>
+                  </tr>
+                  <tr className="border-0">
+                    <td className="w-20 font-bold">Paid</td>
+                    <td></td>
+                    <td className="font-bold">$15.00</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full flex-col gap-5 lg:w-1/4">
+          <div className="flex w-full flex-col gap-5 rounded-xl bg-white p-5 drop-shadow-xl">
+            <h4>Notes</h4>
+            <p className="text-sm">No notes for customer</p>
+          </div>
+          <div className="flex w-full flex-col gap-5 rounded-xl bg-white p-5 drop-shadow-xl">
+            <h4>Customer</h4>
+            <p className="text-sm">John Doe</p>
+            <h4>Contact Information</h4>
+            <p className="text-sm">
+              johndoe@example.com
+              <br />
+              +1 123 123456789
+            </p>
+            <h4>Shipping Address</h4>
+            <p className="text-sm">
+              123, New York City, New York, <br />
+              NY-123456 <br /> United State
+            </p>
+            <h4>Billing Address</h4>
+            <p className="text-sm">
+              123, New York City, New York, <br />
+              NY-123456 <br /> United State
+            </p>
+          </div>
+          <div className="flex w-full flex-col gap-5 rounded-xl bg-white p-5 drop-shadow-xl">
+            <h4>Important</h4>
+            <p className="text-sm">
+              Our product prices can chnage depending on where the order is
+              fulfilled and whichcurrency you use to pay for it. For example, if
+              you order a t-shirt that we only fulfill in the US and you pay for
+              it in EUR, the price you pay would be our USD price converted to
+              EUR.
+            </p>
+          </div>
         </div>
       </div>
     </div>
